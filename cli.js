b5b52c8fe76af1477284eded8fbdc303221bf9ab
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-04-18 08:14:42 +0100

    Initial commit
    Correctly handle thousands and only match bullet-list links (+2 squashed commits)
    Squashed commits:
    [69ffdc6] Formats numbers correctly and handles zero stargazers
    [a8827d1] Moved file handling to the CLI

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ const awesomeStars = require('./index');
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
+const Q = require('q');
 
 const packageConfig = fs.readFileSync(path.join(__dirname, 'package.json'));
 
@@ -14,16 +15,22 @@ program
   .option('-p, --password [string]', 'GitHub password or token')
   .parse(process.argv);
 
-if (program.infile && program.outfile && program.username && program.password) {
-  var bar;
-  awesomeStars(program.infile, program.outfile, program.username, program.password,
-    (count) => {
-      if (count) {
-        bar = new ProgressBar('Fetching stars: [:bar] :percent', { total: count, width: 30 });
-      } else {
-        bar.tick();
-      }
-    });
-} else {
+if (!program.infile || !program.outfile || !program.username || !program.password) {
   console.error('All parameters are mandatory');
+  process.exit()
 }
+
+Q.nfcall(fs.readFile, program.infile, 'utf8')
+  .then(markdown => {
+    var bar;
+    return awesomeStars(markdown, program.username, program.password,
+      (count) => {
+        if (count) {
+          bar = new ProgressBar('Fetching stars: [:bar] :percent', { total: count, width: 30 });
+        } else {
+          bar.tick();
+        }
+      });
+  })
+  .then(transformedMarkdown => Q.nfcall(fs.writeFile, program.outfile, transformedMarkdown))
+  .catch(err => console.error(err));
